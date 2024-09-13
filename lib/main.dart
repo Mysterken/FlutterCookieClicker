593681a8e7deb0bac5a5dd7448603bf7555aef60
Mysterken <Mysterkenzo@gmail.com>
2024-09-13 09:11:11 +0200

    sync

--- lib/main.dart
+++ lib/main.dart
@@ -13,58 +13,51 @@ void main() async {
   final storageService = StorageService();
   await storageService.init();
 
+  final achievements = [
+    Achievement(
+      title: 'First Cookie',
+      description: 'Bake your first cookie',
+      type: AchievementType.cookieCount,
+      threshold: 1,
+      icon: 'assets/icons/first_cookie.png',
+    ),
+    Achievement(
+      title: 'Hundred Cookies',
+      description: 'Bake 100 cookies',
+      type: AchievementType.cookieCount,
+      threshold: 100,
+      icon: 'assets/icons/hundred_cookies.png',
+    ),
+    Achievement(
+      title: 'First Upgrade',
+      description: 'Purchase your first upgrade',
+      type: AchievementType.upgradeCount,
+      threshold: 1,
+      icon: 'assets/icons/first_upgrade.png',
+    ),
+    Achievement(
+      title: 'Cookie Factory',
+      description: 'Reach 10 cookies per second',
+      type: AchievementType.cookiesPerSecond,
+      threshold: 10,
+      icon: 'assets/icons/cookie_factory.png',
+    ),
+  ];
+
   final gameService = GameService(
-    upgrades: [],
-    achievements: [
-      Achievement(
-        title: 'First Cookie',
-        description: 'Bake your first cookie',
-        type: AchievementType.cookieCount,
-        threshold: 1,
-        icon: 'assets/icons/first_cookie.png',
-      ),
-      Achievement(
-        title: 'Hundred Cookies',
-        description: 'Bake 100 cookies',
-        type: AchievementType.cookieCount,
-        threshold: 100,
-        icon: 'assets/icons/hundred_cookies.png',
-      ),
-      Achievement(
-        title: 'First Upgrade',
-        description: 'Purchase your first upgrade',
-        type: AchievementType.upgradeCount,
-        threshold: 1,
-        icon: 'assets/icons/first_upgrade.png',
-      ),
-      Achievement(
-        title: 'Cookie Factory',
-        description: 'Reach 10 cookies per second',
-        type: AchievementType.cookiesPerSecond,
-        threshold: 10,
-        icon: 'assets/icons/cookie_factory.png',
-      ),
-    ],
-    storageService: storageService,
-  );
+      upgrades: [], achievements: achievements, storageService: storageService);
 
   final soundService = SoundService();
 
-  runApp(MyApp(
-    gameService: gameService,
-    soundService: soundService,
-  ));
+  runApp(MyApp(gameService: gameService, soundService: soundService));
 }
 
 class MyApp extends StatelessWidget {
   final GameService gameService;
   final SoundService soundService;
 
-  const MyApp({
-    Key? key,
-    required this.gameService,
-    required this.soundService,
-  }) : super(key: key);
+  const MyApp(
+      {super.key, required this.gameService, required this.soundService});
 
   @override
   Widget build(BuildContext context) {
